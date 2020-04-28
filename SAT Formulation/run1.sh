4725ce28885e3084ff1bdc52fd43432fff8ba26f
Dharani-17 <63633647+Dharani-17@users.noreply.github.com>
2020-04-28 14:33:47 +0530

    run1.sh

--- SAT Formulation/run1.sh
+++ SAT Formulation/run1.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
 
+#run1.sh test
 ## This is to run the first program SAT.java
 
-if [ -e "SAT.class" ]; then
+if [ -e "SAT.class" ]; 
+then
     java SAT $1
 else
     echo "First run build.sh to compile the code"
